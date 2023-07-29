a5709c2ecaec4f7da04ab69b04200e59d0a2768a
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-29 01:04:14 -0700

    fixing

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,6 +11,7 @@
 void bubble_sort(int *array, size_t size)
 {
 	unsigned int i = 0;
+	unsigned int j;
 	int tmp;
 	int swap_occurred;
 
@@ -19,7 +20,7 @@ void bubble_sort(int *array, size_t size)
 
 	do {
 		swap_occurred = 0;
-		for (unsigned int j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
