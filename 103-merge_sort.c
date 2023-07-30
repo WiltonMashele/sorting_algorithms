1871554d6828a25868f32264c402319a78e30429
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-30 11:28:28 -0700

    fixing

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,6 +1,7 @@
 #include "sort.h"
 
-void combine_arrays(int *partial, int *temp, size_t start, size_t middle, size_t end);
+void combine_arrays(int *partial, int *temp, size_t start, size_t middle,
+			size_t end);
 void recursive_merge_sort(int *partial, int *temp, size_t start, size_t end);
 void merge_sort(int *array, size_t size);
 
@@ -9,10 +10,12 @@ void merge_sort(int *array, size_t size);
  * @partial: A part of the array that needs to be sorted.
  * @temp: Temporary storage for the sorting process.
  * @start: The starting index of the first array.
- * @middle: The ending index of the first array and starting index of the second array.
+ * @middle: The ending index of the first array and starting index of the
+ *	second array.
  * @end: The ending index of the second array.
  */
-void combine_arrays(int *partial, int *temp, size_t start, size_t middle, size_t end)
+void combine_arrays(int *partial, int *temp, size_t start, size_t middle,
+			size_t end)
 {
 	size_t i, j, k = 0;
 
